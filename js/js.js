da8af236e8c7442e61385444f1e46a4e8e3357f8
IvanSmolyk <39488193+IvanSmolyk@users.noreply.github.com>
2018-06-06 15:22:20 +0300

    1

--- js/js.js
+++ js/js.js
@@ -10,8 +10,16 @@ window.addeventasync = function () {
 
 document.getElementById('back').oninput = invest;
 
-function invest(){
-    var summ =  geElementById('amount');
-    console.log = this.value;
+function invest(){    
+    var amount =  document.getElementById('amount');
+    var prod =  document.getElementById('prod');
+    var adv =  document.getElementById('adv');
+    var profit =  document.getElementById('profit');
+    var total =  document.getElementById('total');
+    amount.innerHTML = '$' + this.value;
+    prod.innerHTML = '$' + Math.round(this.value * 0.9);
+    adv.innerHTML = '$' + Math.round(this.value * 0.1);
+    profit.innerHTML = '$' + this.value * 3;
+    total.innerHTML = '$' + this.value * 2;
     
 }
