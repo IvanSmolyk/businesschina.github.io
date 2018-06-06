d76893e0f0e7aebdf7dc0256a940031bc72f32d7
IvanSmolyk <39488193+IvanSmolyk@users.noreply.github.com>
2018-06-06 13:17:43 +0300

    1

---
+++ js/js.js
@@ -0,0 +1,17 @@
+window.addeventasync = function () {
+    addeventatc.settings({
+        dropdown: {
+            order: "google,appleical,outlook,yahoo,outlookcom"
+        },
+        css: false
+    });
+};
+
+
+document.getElementById('back').oninput = invest;
+
+function invest(){
+    var summ =  geElementById('amount');
+    console.log = this.value;
+    
+}
