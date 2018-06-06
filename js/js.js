e77b668e8d52ede57e79481b5398200aca702598
IvanSmolyk <39488193+IvanSmolyk@users.noreply.github.com>
2018-06-06 16:59:02 +0300

    3

--- js/js.js
+++ js/js.js
@@ -13,11 +13,13 @@ document.getElementById('back').oninput = invest;
 function invest(){    
     var amount =  document.getElementById('amount');
     var prod =  document.getElementById('prod');
+    var clone = document.getElementById('clone');
     var adver =  document.getElementById('adver');
     var profit =  document.getElementById('profit');
     var total =  document.getElementById('total');
     amount.innerHTML = '$' + this.value;
     prod.innerHTML = '$' + (this.value * 0.9).toFixed(2);
+    clone.innerHTML = '$' + (this.value * 0.1).toFixed(2);
     adver.innerHTML = '$' + (this.value * 0.1).toFixed(2);
     profit.innerHTML = '$' + this.value * 3;
     total.innerHTML = '$' + this.value * 2;
